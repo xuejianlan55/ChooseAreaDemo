9d3a2536d87bd9943d2e529ca969022450e753e0
FuccLetMeBe <283787908@qq.com>
2016-10-26 09:47:33 +0800

    Demo

---
+++ ChooseArea/BaseController.h
@@ -0,0 +1,13 @@
+//
+//  BaseController.h
+//  ChooseArea
+//
+//  Created by 朱帅 on 2016/10/25.
+//  Copyright © 2016年 zhushuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseController : UIViewController
+
+@end
